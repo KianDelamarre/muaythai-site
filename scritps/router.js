0d29b957155720bad736206b996c10a69df6454f
KianDelamarre <kian.delamarre04@gmail.com>
2025-04-27 20:42:23 +0100

    changed slideshow to scrollview instead but currently cannot figure out infinite scroll

--- scritps/router.js
+++ scritps/router.js
@@ -1,41 +1,113 @@
-const navbar=document.getElementById('nav-locations')
+const navbar = document.getElementById('nav-locations')
 
-function openSidebar(){
+function openSidebar() {
   navbar.classList.add('show')
 }
 
-function closeSidebar(){
+function closeSidebar() {
   navbar.classList.remove('show')
 }
 
 const route = (event) => {
-    event = event || window.event;
-    event.preventDefault();
-    window.history.pushState({}, "", event.target.href);
-    handleLocation();
+  event = event || window.event;
+  event.preventDefault();
+  window.history.pushState({}, "", event.target.href);
+  handleLocation();
 };
 
+
 const routes = {
-    404: "/pages/404.html",
-    "/": "/pages/home.html",
-    "/about": "/pages/about.html",
-    "/contact": "/pages/contact.html",
-    "/classes": "/pages/classes.html",
-    "/info": "/pages/info.html",
+  404: "/pages/404.html",
+  "/": "/pages/home.html",
+  "/about": "/pages/about.html",
+  "/contact": "/pages/contact.html",
+  "/classes": "/pages/classes.html",
+  "/info": "/pages/info.html",
 }
 
 const handleLocation = async () => {
-    const path = window.location.pathname;
-    const route = routes[path] || routes[404];
-    const html = await fetch(route).then((data) => data.text());
-    document.getElementById("main-content").innerHTML = html;
-    
-      if (path === "/") {
-        initSlideshowButtons();
-        initSlideShowSwipe();
-    }
+  const path = window.location.pathname;
+  const route = routes[path] || routes[404];
+  const html = await fetch(route).then((data) => data.text());
+  document.getElementById("main-content").innerHTML = html;
+
+  if (path === "/") {
+    // initSlideshowButtons();
+    initButton();
+  }
 };
 
+function initButton() {
+  const slideShow = document.getElementById("coaches-slideshoww");
+  const nextBtn = document.querySelector("[data-slideshow-next]");
+  const prevBtn = document.querySelector("[data-slideshow-prev]");
+
+  let stopScroll = false;
+
+
+  function handleInfiniteScroll() {
+
+
+
+    if (slideShow.scrollLeft === slideShow.scrollWidth - slideShow.clientWidth) {
+      stopScroll = true;
+      slideShow.scrollTo({
+        left: -slideShow.scrollWidth,
+        behavior: "smooth"
+      });
+    }
+    else if (slideShow.scrollLeft === 0) {
+      stopScroll = true;
+      slideShow.scrollTo({
+        left: slideShow.scrollWidth,
+        behavior: "smooth"
+      });
+    }
+
+    if (stopScroll) return;
+  }
+
+  nextBtn.addEventListener("click", () => {
+    console.log("next button clicked");
+    if (slideShow.scrollLeft === slideShow.scrollWidth - slideShow.clientWidth) {
+      slideShow.scrollTo({
+        left: -slideShow.scrollWidth,
+        behavior: "smooth"
+      })
+    }
+    else {
+      slideShow.scrollBy({
+        left: window.innerWidth,
+        behavior: "smooth"
+      })
+    }
+    scrollNext();
+  });
+
+  prevBtn.addEventListener("click", () => {
+    console.log("prev button clicked");
+    if (slideShow.scrollLeft === 0) {
+      slideShow.scrollTo({
+        left: slideShow.scrollWidth,
+        behavior: "smooth"
+      })
+    }
+    else {
+      slideShow.scrollBy({
+        left: -window.innerWidth,
+        behavior: "smooth"
+      })
+    }
+  });
+
+  slideShow.addEventListener('scroll', () => {
+    // if (slideShow.scrollLeft !== 0 && slideShow.scrollLeft !== slideShow.scrollWidth - slideShow.clientWidth) {
+    //   stopScroll = false;
+    // }
+    handleInfiniteScroll();
+    stopScroll = false;
+  });
+}
 
 function initSlideshowButtons() {
   const buttons = document.querySelectorAll("[data-slideshow-button]");
@@ -60,93 +132,6 @@ function initSlideshowButtons() {
       delete activeSlide.dataset.active;
     });
   });
-}
-
-function initSlideShowSwipe() {
-  let touchArea = document.getElementById("coaches-slideshow");
-
-  let mouseX,
-    intialX = 0;
-  let mouseY,
-    initalY = 0;
-  let isSwiped;
-
-  let events = {
-    mouse: {
-      down: "mousedown",
-      move: "mousemove",
-      up: "mouseup",
-    },
-    touch: {
-      down: "touchstart",
-      move: "touchmove",
-      up: "touchend",
-    },
-  };
-
-  let deviceType = "";
-
-  const isTouchDevice = () => {
-    try {
-      document.createEvent("TouchEvent");
-      deviceType = "touch";
-      return true;
-    }
-    catch (e) {
-      deviveType = "mouse";
-      return false;
-    }
-  };
-
-  console.log("is touch device? " + isTouchDevice());
-
-  let rectLeft = touchArea.getBoundingClientRect().left;
-  let rectTop = touchArea.getBoundingClientRect().top; 
-
-  const getXY = (e) => {
-    mouseX = (!isTouchDevice() ? e.pageX : e.touches[0].pageX) - rectLeft;
-    mouseY = (!isTouchDevice() ? e.pageY : e.touches[0].pageY) - rectTop;
-  };
-
-  // isTouchDevice();
-
-  touchArea.addEventListener(events[deviveType].down,
-    (event) => {
-      isSwiped = true;
-
-      getXY(event);
-      console.log(mouseX, mouseY);
-      intialX = mouseX;
-      initalY = mouseY;
-    });
-  
-  touchArea.addEventListener(events[deviceType].move,
-    (event) => {
-      if (!isTouchDevice()) {
-        event.preventDefault();
-      }
-      if (isSwiped) {
-        getXY(event);
-        let diffX = mouseX - intialX;
-        let diffY = mouseY - initalY;
-        if (Math.abs(diffY) > Math.abs(diffX)) {
-          console.log("swiped vert");
-        }
-        else {
-          console.log("swiped hor");
-        }
-      }
-    }
-  );
-
-  touchArea.addEventListener(events[deviveType].up, () => {
-    isSwiped = false;
-  });
-
-  touchArea.addEventListener("mouseleave", () => {
-    isSwiped = false;
-  });
-
 }
 
 window.onpopstate = handleLocation;
