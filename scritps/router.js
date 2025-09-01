ab162fa42c521e6bab970d8ac2e221f09ffa7b02
Kian Delamarre <kian.delamarre04@gmail.com>
2025-09-01 11:10:19 +0100

    fixed infinite scroll issue due to scrollLeft having too many decimals

--- scritps/router.js
+++ scritps/router.js
@@ -8,6 +8,7 @@ function closeSidebar() {
   navbar.classList.remove('show')
 }
 
+/////////   router //////////////////////
 const route = (event) => {
   event = event || window.event;
   event.preventDefault();
@@ -23,6 +24,7 @@ const routes = {
   "/contact": "/pages/contact.html",
   "/classes": "/pages/classes.html",
   "/info": "/pages/info.html",
+  "/blog": "/pages/blog.html"
 }
 
 const handleLocation = async () => {
@@ -35,11 +37,17 @@ const handleLocation = async () => {
 
 
   if (path === "/") {
-    // initSlideshowButtons();
-    initButton();
+    initButton();    // initalise slideshow buttons only when on home page
+    console.log("slideshow");
+  }
+  else if (path === "/blog") {
+    initBlogSection();
   }
 };
 
+/////////   router //////////////////////
+
+////////   coaches slideshow section /////////////////
 function initButton() {
   const slideShow = document.getElementById("coaches-slideshoww");
   const nextBtn = document.querySelector("[data-slideshow-next]");
@@ -50,6 +58,7 @@ function initButton() {
   let autoScrollTimer = setInterval(autoScroll, 20000);
   let isScrolling;
 
+  //sets slideshow back to first slide 
   slideShow.scrollTo({
     left: 1 * slideShow.clientWidth,
     behavior: "instant"
@@ -60,18 +69,18 @@ function initButton() {
     console.log(slideShow.scrollWidth);
     console.log(slideShow.clientWidth);
     console.log(slideShow.scrollLeft);
+    //if scrolled to end of slide show - duplicate buffer slide
+    if (Math.round(slideShow.scrollLeft) === slideShow.scrollWidth - slideShow.clientWidth) {
 
-    if (slideShow.scrollLeft === slideShow.scrollWidth - slideShow.clientWidth) {
-
-      console.log("infite scroll");
+      console.log("reached end, resetting to 1st slide");
       slideShow.scrollTo({
-        left: 1 * slideShow.clientWidth,
+        left: slideShow.clientWidth,
         behavior: "instant"
       });
     }
     else if (slideShow.scrollLeft === 0) {
 
-      console.log("infite scroll");
+      console.log("scrolled back to end");
       slideShow.scrollTo({
         left: secondLastIndex * slideShow.clientWidth,
         behavior: "instant"
@@ -125,7 +134,229 @@ function initButton() {
   }
 
 
+  ///////////   coaches slideshow section /////////////////
+
+
 }
+
+function initBlogSection() {
+  const blogPostsSection = document.getElementById('blog-posts-section');
+  let skip = 0;
+  const load = 10;  // load 10 posts at a time
+  const loadMoreBtn = document.getElementById('load-more');
+  const addPostBtn = document.getElementById('post');
+  const createPostButton = document.getElementById('createPostButton');
+  const createPostForm = document.getElementById('createPostForm');
+  const activateBtn = document.getElementById('activate');
+  let loading = false;
+
+
+  /////////////////////// blog page /////////////////////////////////////
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting && !loading) {
+        // When last post comes into view, load more posts
+        loadPosts();
+      }
+    });
+  }, {
+    rootMargin: '100px',  // start loading a bit before the element fully appears
+  });
+
+
+
+  function loadPosts() {
+    if (loading) return;
+    loading = true;
+
+    fetch(`http://127.0.0.1:3000/blog?load=${load}&skip=${skip}`, {
+      method: "GET"
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.length === 0) {
+          // No more posts
+
+          observer.disconnect();
+
+          if (loadMoreBtn) {
+            loadMoreBtn.disabled = true;
+            loadMoreBtn.textContent = "No more posts";
+          }
+          loading = false;
+          return;
+        }
+
+        data.forEach(post => {
+          console.log(post);
+          const div = document.createElement('div');
+          div.className = "post";
+          div.id = `post-${post.id}`
+
+          const title = document.createElement('h2');
+          title.textContent = post.title;
+
+          // const imgContainer = document.createElement('img-container');
+          const imgContainer = document.createElement('div');
+          const img = document.createElement('img');
+
+
+          img.src = post.img_url;
+          img.alt = post.title;
+          img.className = "post-img";
+          imgContainer.className = "img-container";
+          imgContainer.appendChild(img);
+
+          const text = document.createElement('p');
+          text.textContent = post.text;
+
+          const deletePostButton = document.createElement('button');
+          deletePostButton.textContent = "X";
+
+          // div.appendChild(title);
+
+
+          div.appendChild(imgContainer);
+          // div.appendChild(img);
+          // console.log("tried to load image");
+
+          div.appendChild(text);
+
+          // // // // //div.appendChild(deletePostButton);
+
+          blogPostsSection.appendChild(div);
+          deletePostButton.addEventListener('click', () => {
+            // console.log(div.id);
+            deletePost(post.id);
+
+          })
+        });
+
+        skip += load;
+
+        observer.disconnect();
+        // Observe the new last post
+        const posts = document.querySelectorAll('.post');
+        const lastPost = posts[posts.length - 1];
+        if (lastPost) {
+          observer.observe(lastPost);
+        }
+
+        loading = false;
+      })
+      .catch(err => {
+        console.error('Failed to fetch blog data:', err);
+      });
+
+
+  };
+
+  async function addRandomPost() {
+    const title = "new post";
+    const text = "new post";
+    const randomInt = Math.floor(Math.random() * (210 - 190 + 1)) + 190;
+    let img_url = `https://picsum.photos/${randomInt}`;
+
+    try {
+      const response = await fetch("http://localhost:3000/postrandom", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          title,
+          text,
+          img_url
+        })
+      });
+
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Upload failed:", error);
+    }
+  };
+
+  async function addPost() {
+    const title = document.getElementById('createPostTitleInput').value;
+    const text = document.getElementById('createPostTextInput').value;
+    // const randomInt = Math.floor(Math.random() * (210 - 190 + 1)) + 190;
+    // let img_url = `https://picsum.photos/${randomInt}`;
+
+
+    const fileInput = document.getElementById('uploadImg');
+    const file = fileInput.files[0];
+
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("title", title);
+    formData.append("text", text);
+
+
+    try {
+      const response = await fetch("http://localhost:3000/post", {
+        method: "POST",
+        body: formData
+      });
+
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Upload failed:", error);
+    }
+  };
+
+  // Load initial posts
+  loadPosts();
+
+  // Button to load more posts
+  loadMoreBtn.addEventListener('click', () => {
+    loadPosts();
+  })
+
+
+
+  addPostBtn.addEventListener('click', () => {
+    addRandomPost();
+  })
+
+
+  createPostForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    let title = document.getElementById('createPostTitleInput').value;
+    let text = document.getElementById('createPostTextInput').value;
+    console.log(title, text);
+    addPost();
+  })
+
+  function deletePost(id) {
+    console.log('post-' + id);
+    const confirmed = confirm("Are you sure you want to do this?");
+    if (!confirmed) {
+      return;
+    }
+
+    fetch(`http://localhost:3000/delete?id=${id}`, {
+      method: "DELETE"
+    })
+      .then(res => {
+        if (!res.ok) {
+          throw new Error('network response not ok');
+        }
+        return response.json;
+      })
+      .then(data => {
+        console.log('delete successful:', data);
+      })
+      .catch(error => {
+        console.error('delete failed:', error);
+      })
+  }
+
+};
+
+
+
+
+/////////////////////// blog page /////////////////////////////////////
 
 window.onpopstate = handleLocation;
 window.route = route;
